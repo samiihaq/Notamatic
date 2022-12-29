8450ddbbcd5bd530943045087f3ab30df1b7c0b1
samii321 <samihaq1519@gmail.com>
2022-12-30 00:08:25 +0500

    Switching to ionic storage instead.

--- notamatic-app/src/app/pages/login/login.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$email = $_POST['email'];
-$username= $_POST['username'];
-$password= $_POST['password'];
-$institute= $_POST['institute'];
-
-$conn = new mysqli('localhost', 'root', '', 'notamatic')
